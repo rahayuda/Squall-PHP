efbb97f43410169978d370f31100ac557d1a210f
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-12-18 12:28:07 +0800

    Update deleteItem.js

--- api/deleteItem.js
+++ api/deleteItem.js
@@ -1,18 +1,7 @@
-// deleteItem.js
-let items = [];  // Array untuk menyimpan item
-
 module.exports = (req, res) => {
     const { id } = req.query;
 
-    // Mencari indeks item dengan id yang sesuai dalam array
-    const indexToDelete = items.findIndex(item => item.id === id);
+    // In a real application, you would delete the item from the database here
 
-    if (indexToDelete !== -1) {
-        // Menghapus item dari array jika ditemukan
-        items.splice(indexToDelete, 1);
-        res.status(200).send('Item deleted successfully');
-    } else {
-        // Memberikan respons bahwa item tidak ditemukan
-        res.status(404).send('Item not found');
-    }
+    res.status(200).send('Item deleted successfully');
 };
