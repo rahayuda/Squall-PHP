6c59ccd521c85a9820768dcf0c9c5cefcebd5089
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-12-18 12:28:30 +0800

    Update addItem.js

--- api/addItem.js
+++ api/addItem.js
@@ -1,18 +1,11 @@
-// addItem.js
-let items = [];  // Array untuk menyimpan item
-
 module.exports = (req, res) => {
     const { itemName } = req.body;
 
-    // Membuat objek item baru
+    // In a real application, you would use a proper database here
     const newItem = {
         id: Date.now().toString(),
         itemName,
     };
 
-    // Menambahkan item ke dalam array
-    items.push(newItem);
-
-    // Mengembalikan item baru
     res.status(200).json(newItem);
 };
