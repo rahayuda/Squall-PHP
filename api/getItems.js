6d1e2de633b1fe5c39b8413db0133c9c43b4dcda
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-12-18 12:29:03 +0800

    Update getItems.js

--- api/getItems.js
+++ api/getItems.js
@@ -1,7 +1,9 @@
-// getItems.js
-let items = [];  // Array untuk menyimpan item
-
 module.exports = (req, res) => {
-    // Mengembalikan seluruh array item
+    // In a real application, you would fetch items from the database here
+    const items = [
+        { id: '1', itemName: 'Item 1' },
+        { id: '2', itemName: 'Item 2' },
+    ];
+
     res.status(200).json(items);
 };
